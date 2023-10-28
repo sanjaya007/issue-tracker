0b3f14572f933bc43fb55d900ff03a3d6a1b286d
sanjaya007 <paudelsanjaya702@gmail.com>
2023-10-28 16:48:49 +0545

    setup radix ui

---
+++ app/issues/page.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Button } from "@radix-ui/themes";
+
+const IssuesPage = () => {
+  return (
+    <div>
+      <Button>New Issue</Button>
+    </div>
+  );
+};
+
+export default IssuesPage;
