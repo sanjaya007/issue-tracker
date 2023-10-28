0b71f64b2b61e01c395d060bc4384c08a9441c3b
sanjaya007 <paudelsanjaya702@gmail.com>
2023-10-28 18:47:35 +0545

    new issue page created

--- app/api/issues/route.ts
+++ app/api/issues/route.ts
@@ -7,7 +7,7 @@ const createIssueSchema = z.object({
   description: z.string().min(1),
 });
 
-async function POST(request: NextRequest) {
+export async function POST(request: NextRequest) {
   const body = await request.json();
   const validation = createIssueSchema.safeParse(body);
 
